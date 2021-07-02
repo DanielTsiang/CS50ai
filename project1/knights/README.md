dceba633ae18672bba94c37c1cde40819b1e191f
Daniel Tsiang <74436899+DanielTsiang@users.noreply.github.com>
2021-07-02 14:48:34 +0100

    Initial upload of project1 knights

---
+++ project1/knights/README.md
@@ -0,0 +1,35 @@
+# "Knights and Knaves" Puzzles
+
+### Description
+A program that solves logic puzzles. The puzzles are represented using propositional logic, enabling the AI to solve them using a model-checking algorithm.
+
+### Background
+In a Knights and Knaves puzzle, the following information is given: Each character is either a knight or a knave. A knight will always tell the truth: if knight states a sentence, then that sentence is true.
+Conversely, a knave will always lie: if a knave states a sentence, then that sentence is false.
+
+The objective of the puzzle is, given a set of sentences spoken by each of the characters, determine, for each character, whether that character is a knight or a knave.
+
+<p align="center">
+  <img width="318" src="https://user-images.githubusercontent.com/74436899/124282635-f46c6100-db42-11eb-905f-eaf3eba882b1.png">
+</p>
+
+### Getting Started
+1. Run ```python puzzle.py``` to solve the puzzles and view the answers in the terminal.
+
+### Puzzles
+* Puzzle 0 contains a single character, A.
+    * A says “I am both a knight and a knave.”
+* Puzzle 1 has two characters: A and B.
+    * A says “We are both knaves.”
+    * B says nothing.
+* Puzzle 2 has two characters: A and B.
+    * A says “We are the same kind.”
+    * B says “We are of different kinds.”
+* Puzzle 3 has three characters: A, B, and C.
+    * A says either “I am a knight.” or “I am a knave.”, but you don’t know which.
+    * B says “A said ‘I am a knave.’”
+    * B then says “C is a knave.”
+    * C says “A is a knight.”
+
+### Technologies Used
+* Python
---
+++ project1/knights/puzzle.py
@@ -0,0 +1,110 @@
+from logic import *
+
+AKnight = Symbol("A is a Knight")
+AKnave = Symbol("A is a Knave")
+
+BKnight = Symbol("B is a Knight")
+BKnave = Symbol("B is a Knave")
+
+CKnight = Symbol("C is a Knight")
+CKnave = Symbol("C is a Knave")
+
+# Puzzle 0
+# A says "I am both a knight and a knave."
+sentence0 = And(AKnight, AKnave)
+knowledge0 = And(
+    # each person is either a knight or a knave
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Or(CKnight, CKnave),
+    # each person cannot be both
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    Not(And(CKnight, CKnave)),
+    # if person is a knight, then sentence is true, else it is false
+    Biconditional(AKnight, sentence0)
+)
+
+# Puzzle 1
+# A says "We are both knaves."
+# B says nothing.
+sentence1 = And(AKnave, BKnave)
+knowledge1 = And(
+    # each person is either a knight or a knave
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Or(CKnight, CKnave),
+    # each person cannot be both
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    Not(And(CKnight, CKnave)),
+    # if person is a knight, then sentence is true, else it is false
+    Biconditional(AKnight, sentence1)
+)
+
+# Puzzle 2
+# A says "We are the same kind."
+# B says "We are of different kinds."
+sentence2A = Or(And(AKnight, BKnight), And(AKnave, BKnave))
+sentence2B = Or(And(AKnight, BKnave), And(AKnave, BKnight))
+knowledge2 = And(
+    # each person is either a knight or a knave
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Or(CKnight, CKnave),
+    # each person cannot be both
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    Not(And(CKnight, CKnave)),
+    # if person is a knight, then sentence is true, else it is false
+    Biconditional(AKnight, sentence2A),
+    Biconditional(BKnight, sentence2B)
+)
+
+# Puzzle 3
+# A says either "I am a knight." or "I am a knave.", but you don't know which.
+# B says "A said 'I am a knave'."
+# B says "C is a knave."
+# C says "A is a knight."
+sentence3A = Biconditional(AKnight, Not(AKnave))
+sentence3B = And(
+    Biconditional(BKnight, AKnave),
+    CKnave
+)
+sentence3C = AKnight
+knowledge3 = And(
+    # each person is either a knight or a knave
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Or(CKnight, CKnave),
+    # each person cannot be both
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    Not(And(CKnight, CKnave)),
+    # if person is a knight, then sentence is true, else it is false
+    Biconditional(AKnight, sentence3A),
+    Biconditional(BKnight, sentence3B),
+    Biconditional(CKnight, sentence3C)
+)
+
+
+def main():
+    symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
+    puzzles = [
+        ("Puzzle 0", knowledge0),
+        ("Puzzle 1", knowledge1),
+        ("Puzzle 2", knowledge2),
+        ("Puzzle 3", knowledge3)
+    ]
+    for puzzle, knowledge in puzzles:
+        print(puzzle)
+        if len(knowledge.conjuncts) == 0:
+            print("    Not yet implemented.")
+        else:
+            for symbol in symbols:
+                if model_check(knowledge, symbol):
+                    print(f"    {symbol}")
+
+
+if __name__ == "__main__":
+    main()

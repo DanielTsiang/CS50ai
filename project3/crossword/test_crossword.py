27482be46412a2e4c86f086135ba9c2b3d5acc7f
DanielTsiang <dan_tsiang@hotmail.co.uk>
2021-10-31 11:56:32 +0000

    Add unit test for all 3 data sets, and assert correct length for crossword generation

--- project3/crossword/test_crossword.py
+++ project3/crossword/test_crossword.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 from pathlib import Path
 
 from generate import generate_crossword
-from test_resources.expected_results import data1
+from test_resources.expected_results import data0, data1
 
 
 class Test(unittest.TestCase):
@@ -15,20 +15,34 @@ class Test(unittest.TestCase):
 
     def setUp(self):
         self.data_set = {
-            "data1": data1,
+            "data0": [31, data0],
+            "data1": [136, data1],
+            "data2": 49
         }
 
     def test_generate(self):
-        for i, data in enumerate(self.data_set, 1):
+        for i, data in enumerate(self.data_set):
             # Mock `sys.argv` using `patch.object()`
             with patch.object(sys, "argv", [Path(__file__).name, f"data/structure{i}.txt", f"data/words{i}.txt"]):
                 with self.subTest(data=data):
                     creator, assignment, _ = generate_crossword()
                     result = creator.string(assignment)
-                    expected = self.data_set[data]
-                    message = f"\nFailed for structure '{sys.argv[1]}' and words '{sys.argv[2]}'." \
-                              f"\nExpected {expected} but calculated {result}."
-                    self.assertIn(result, expected, message)
+
+                    error_message = f"\nFailed for structure 'data/structure{i}.txt' and words 'data/words{i}.txt'."
+                    if isinstance(self.data_set[data], list):
+                        expected_length = self.data_set[data][0]
+                        expected_crossword = self.data_set[data][1]
+
+                        error_message0 = error_message + f"\nExpected {expected_length} but calculated {len(result)}."
+                        self.assertEqual(expected_length, len(result), error_message0)
+
+                        error_message1 = error_message + f"\nExpected {expected_crossword} but calculated {result}."
+                        self.assertIn(result, expected_crossword, error_message1)
+
+                    else:
+                        expected_length = self.data_set[data]
+                        error_message0 = error_message + f"\nExpected {expected_length} but calculated {len(result)}."
+                        self.assertEqual(expected_length, len(result), error_message0)
 
 
 if __name__ == "__main__":

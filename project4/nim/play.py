f2a8b0dcafc3cf33259fa0e33df94231f955e8c9
DanielTsiang <dan_tsiang@hotmail.co.uk>
2021-11-01 00:41:42 +0000

    Remove test copy in play.py

--- project4/nim/play.py
+++ project4/nim/play.py
@@ -1,9 +1,7 @@
 from nim import train, play
-from copy import deepcopy
 
 # The train function trains an AI by running n simulated games against itself, returning the fully trained AI.
 ai = train(10000)
-ai_copy = deepcopy(ai)
 
 while True:
     # The play function accepts a trained AI as input, and lets a human player play a game of Nim against the AI.

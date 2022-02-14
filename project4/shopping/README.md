f1f157c40667ec9cc0b7726316c76536238f1849
Daniel Tsiang <74436899+DanielTsiang@users.noreply.github.com>
2022-02-14 01:52:34 +0000

    More advanced `sklearn` modelling for shopping classifier (#1)
    * Compare untuned and tuned KNN models
    
    * Use pipeline with `RobustScaler`, simplify evaluation
    
    * Update README.md
    
    * Fix logic in README.md
    
    * Fix tuning descriptions

--- project4/shopping/README.md
+++ project4/shopping/README.md
@@ -1,7 +1,8 @@
 # Shopping
 
 ### Description
-An AI that predicts whether online shopping customers will complete a purchase, by using a nearest-neighbor classifier.
+An AI that predicts whether online shopping customers will complete a purchase,
+by using a tuned nearest-neighbor classifier on a scaled dataset.
 
 <p align="center">
   <img width="500" src="https://user-images.githubusercontent.com/74436899/146847921-a4ce2602-363a-4c06-8d4f-0dc4b2e8a0d5.png">
@@ -15,18 +16,30 @@ An AI that predicts whether online shopping customers will complete a purchase, 
 ### Example
 ```
 $ python shopping.py shopping.csv
-Correct: 4088
-Incorrect: 844
-True Positive Rate: 41.02%
-True Negative Rate: 90.55%
+Accuracy: 88.6%
+True Positive Rate: 52.0%
+True Negative Rate: 95.3%
+Classification metrics:
+               precision    recall  f1-score   support
+
+           0       0.92      0.95      0.93      4169
+           1       0.67      0.52      0.59       763
+
+    accuracy                           0.89      4932
+   macro avg       0.79      0.74      0.76      4932
+weighted avg       0.88      0.89      0.88      4932
 ```
 
 ### Background
-Given information about a user — how many pages they’ve visited, whether they’re shopping on a weekend, what web browser they’re using, etc. — the nearest-neighbor classifier will predict whether the user will make a purchase.
+Given information about a user — how many pages they’ve visited, whether they’re shopping on a weekend,
+what web browser they’re using, etc. — the nearest-neighbor classifier will predict whether the user will make a purchase.
+The number of nearest-neighbors used (i.e. the k-value) is tuned, via cross-validated grid search, to improve the performance of the classifier.
 
 To train the classifier, shopping.csv contains data from a shopping website from about 12,000 users sessions.
+The dataset is scaled using statistics that are robust to outliers, thus further improving the model's prediction performance.
 
-Two values are measured:
+Three values are highlighted in the results:
+* accuracy (i.e. the correct predictions)
 * sensitivity (also known as the “true positive rate”)
 * specificity (also known as the “true negative rate”).
 
@@ -34,7 +47,7 @@ Sensitivity refers to the proportion of positive examples that were correctly id
 
 Specificity refers to the proportion of negative examples that were correctly identified: in this case, the proportion of users who did not go through with a purchase who were correctly identified.
 
-The goal is to build a classifier that performs reasonably on both metrics.
+The goal is to build a classifier that performs reasonably on the classification metrics.
 
 ### Technologies Used
 * Python
--- project4/shopping/shopping.py
+++ project4/shopping/shopping.py
@@ -1,11 +1,15 @@
-import csv
-import sys
 import calendar
+import csv
+import numpy as np
+import sys
 
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import RobustScaler
 
+CROSS_VALIDATION_SIZE = 5
 TEST_SIZE = 0.4
 
 
@@ -15,43 +19,71 @@ def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: python shopping.py data")
 
-    y_test, predictions, sensitivity, specificity = find_predictions()
+    y_test, y_pred = predict_labels()
 
-    # Number of correctly classified samples
-    correct = accuracy_score(y_test, predictions, normalize=False)
+    # Evaluate model performance
+    report = classification_report(y_test, y_pred, output_dict=True)
 
-    # Number of incorrectly classified samples
-    incorrect = len(predictions) - correct
+    # Extract results
+    # `accuracy` represents the proportion of labels that were accurately identified
+    print(f"Accuracy: {100 * report['accuracy']:.1f}%")
 
-    # Print results
-    print(f"Correct: {correct}")
-    print(f"Incorrect: {incorrect}")
-    print(f"True Positive Rate: {100 * sensitivity:.2f}%")
-    print(f"True Negative Rate: {100 * specificity:.2f}%")
+    # `sensitivity` represents the "true positive rate":
+    # the proportion of actual positive labels that were accurately identified.
+    print(f"True Positive Rate: {100 * report['1']['recall']:.1f}%")
+
+    # `specificity` represents the "true negative rate":
+    # the proportion of actual negative labels that were accurately identified.
+    print(f"True Negative Rate: {100 * report['0']['recall']:.1f}%")
+
+    # Show full classification report
+    print(f"Classification metrics:\n {classification_report(y_test, y_pred)}")
 
 
-def find_predictions():
+def predict_labels() -> tuple[list, list]:
+    """
+    Load and split data into train and test sets.
+    Tune and train model, then use it to make predictions on label classification.
+    """
     # Load data from spreadsheet and split into train and test sets
     evidence, labels = load_data(sys.argv[1])
     X_train, X_test, y_train, y_test = train_test_split(
-        evidence, labels, test_size=TEST_SIZE
+        evidence, labels, test_size=TEST_SIZE, random_state=42, stratify=labels
     )
 
-    # Train model and make predictions
+    # Tune and train model, and use it to make predictions
     model = train_model(X_train, y_train)
-    predictions = model.predict(X_test)
-    sensitivity, specificity = evaluate(y_test, predictions)
+    y_pred = model.predict(X_test)
 
-    return y_test, predictions, sensitivity, specificity
+    return y_test, y_pred
 
 
-def load_data(filename):
+def train_model(X_train: list, y_train: list) -> GridSearchCV:
     """
-    Load shopping data from a CSV file `filename` and convert into a list of
-    evidence lists and a list of labels. Return a tuple (evidence, labels).
+    Given a list of evidence lists (X_train) and a list of labels (y_train),
+    return a fitted k-nearest neighbor model, with a tuned value of k, trained on the data.
+    """
+    # Create pipeline
+    pipeline = Pipeline([("scaler", RobustScaler()),
+                         ("knn", KNeighborsClassifier())])
 
-    evidence should be a list of lists, where each list contains the
-    following values, in order:
+    # Create hyperparameter grid
+    params = {"knn__n_neighbors": np.arange(5, 12)}
+
+    # Use cross-validated grid search to tune k-nearest neighbor model, and then fit the model
+    model_cross_validation = GridSearchCV(pipeline, params, cv=CROSS_VALIDATION_SIZE)
+    model_cross_validation.fit(X_train, y_train)
+
+    # Return fitted model
+    return model_cross_validation
+
+
+def load_data(filename: str) -> tuple[list, list]:
+    """
+    Load shopping data from a CSV file `filename` and convert into a list of evidence lists and a list of labels.
+    Return a tuple (evidence, labels).
+
+    `evidence` should be a list of lists, where each list contains the following values, in order:
         - Administrative, an integer
         - Administrative_Duration, a floating point number
         - Informational, an integer
@@ -70,8 +102,7 @@ def load_data(filename):
         - VisitorType, an integer 0 (not returning) or 1 (returning)
         - Weekend, an integer 0 (if false) or 1 (if true)
 
-    labels should be the corresponding list of labels, where each label
-    is 1 if Revenue is true, and 0 otherwise.
+    labels should be the corresponding list of labels, where each label is 1 if Revenue is true, and 0 otherwise.
     """
     evidence, labels = [], []
 
@@ -99,7 +130,7 @@ def load_data(filename):
                 int(row[11]),   # OperatingSystems
                 int(row[12]),   # Browser
                 int(row[13]),   # Region
-                int(row[14]),   # TrafficTyp
+                int(row[14]),   # TrafficType
                 1 if row[15] == "Returning_Visitor" else 0,  # VisitorType
                 int(row[16] == "TRUE")  # Weekend
             ])
@@ -109,53 +140,6 @@ def load_data(filename):
             )
 
     return evidence, labels
-
-
-def train_model(evidence, labels):
-    """
-    Given a list of evidence lists and a list of labels, return a
-    fitted k-nearest neighbor model (k=1) trained on the data.
-    """
-    model = KNeighborsClassifier(n_neighbors=1)
-    model.fit(evidence, labels)
-
-    # Return fitted model
-    return model
-
-
-def evaluate(labels, predictions):
-    """
-    Given a list of actual labels and a list of predicted labels,
-    return a tuple (sensitivity, specificty).
-
-    Assume each label is either a 1 (positive) or 0 (negative).
-
-    `sensitivity` should be a floating-point value from 0 to 1
-    representing the "true positive rate": the proportion of
-    actual positive labels that were accurately identified.
-
-    `specificity` should be a floating-point value from 0 to 1
-    representing the "true negative rate": the proportion of
-    actual negative labels that were accurately identified.
-    """
-    sensitivity, specificity = 0.0, 0.0
-    positive, negative = 0.0, 0.0
-
-    for label, prediction in zip(labels, predictions):
-        if label == 1:  # if label is positive, then we calculate sensitivity (positive rate)
-            positive += 1
-            if label == prediction:
-                sensitivity += 1
-
-        else:  # if label is negative, then we calculate specificity (negative rate)
-            negative += 1
-            if label == prediction:
-                specificity += 1
-
-    sensitivity /= positive
-    specificity /= negative
-
-    return sensitivity, specificity
 
 
 if __name__ == "__main__":
